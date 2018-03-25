d96cff93971817bb1b12def1923d5d1a611b0a0c
fadhlanalhirzi <fadhlanalhirzi@gmail.com>
2018-03-25 22:03:57 +0700

    push pertama kali

---
+++ app/src/main/java/com/example/fadhlanalhirzi/mfadhlanalhirzi_1202144021_modul5/AddDataActivity.java
@@ -0,0 +1,42 @@
+package com.example.fadhlanalhirzi.mfadhlanalhirzi_1202144021_modul5;
+
+/**
+ * Created by User on 25/03/2018.
+ */
+
+public class AddDataActivity {
+    //deklarasi variable
+    String todo, desc, prior;
+
+    //konstruktor
+    public AddDataActivity(String todo, String desc, String prior){
+        this.todo=todo;
+        this.desc=desc;
+        this.prior=prior;
+    }
+
+    //method setter dan getter untuk to do , description dan priority
+    public String getTodo() {
+        return todo;
+    }
+
+    public void setTodo(String todo) {
+        this.todo = todo;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getPrior() {
+        return prior;
+    }
+
+    public void setPrior(String prior) {
+        this.prior = prior;
+    }
+}
